4c3b5717dd14b0aa6ccaf41049143a3141558a24
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-01 14:45:43 +0400

    работа с переменной num

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let num = 266219
-let numArr = num.toString().split('')
-let numRes = numArr.reduce((res, item) => item * res)
+let numArr = num.toString().split('') // Преобразование числа в строку и разбиение на массив
+let numRes = numArr.reduce((res, item) => item * res) // Перемножение чисел массива с помошью метода reduce
 
-console.log(numRes);
+console.log(numArr);
 
 let numDeg = numRes ** 3
 
 console.log(numDeg);
-console.log(String(numDeg).slice(0, 2));
+console.log(String(numDeg).slice(0, 2)); // Перевод числа в строку и получение двух первых цифр из строки
