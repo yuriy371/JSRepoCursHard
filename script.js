27d75c9e8d03fbb863a9519e59d344547fcb35de
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-01 14:31:16 +0400

    работа с переменной num

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let num = 266219
+let numArr = num.toString().split('')
+let numRes = numArr.reduce((res, item) => item * res)
 
+console.log(numRes);
+
+let numDeg = numRes ** 3
+
+console.log(numDeg);
+console.log(String(numDeg).slice(0, 2));
