975f97e0bfbf15180bd5c6b5e1d75411f1f4af3a
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-04 21:22:23 +0400

    правка условий, добавение значений по умолчанию

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let lang = prompt("Выберите язык", "ru, en")
+let daysRu = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье"
+let daysEn = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"
 
 /* if-else */
 if (lang === "ru") {
-    console.log("Понедельник,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье");
+    console.log(daysRu);
 } else if (lang === "en") {
-    console.log("Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday");
+    console.log(daysEn);
+} else {
+    console.log("Данного языка нет в системе");
 }
 /* ------- */
 
@@ -13,10 +17,13 @@ console.log("End if-else ------------------------");
 /* switch-case */
 switch (lang) {
     case "ru":
-        console.log("Понедельник,Вторник,Среда,Четверг,Пятница,Суббота,Воскресенье");
+        console.log(daysRu);
         break;
     case "en":
-        console.log("Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday");
+        console.log(daysEn);
+        break;
+    default:
+        console.log("Данного языка нет в системе");
         break;
 }
 /* ------------*/
@@ -29,12 +36,13 @@ let arrDay = [
     ["en", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",]]
 ]
 
-res = (arrDay[0][0] == lang) ? console.log(arrDay[0][1]) : console.log(arrDay[1][1]);
+res = (arrDay[0][0] == lang) ? console.log(arrDay[0][1]) :
+    (arrDay[1][0] == lang) ? console.log(arrDay[1][1]) : console.log("Данного языка нет в системе");;
 /* -------- */
 
 console.log("End Mult-Arr ------------------------");
 
 let namePerson = prompt("Введите имя");
 
-resPosition = (namePerson == "Артем") ? console.log("директор") : 
-(namePerson == "Александр") ? console.log("преподаватель") : console.log("студент");
+resPosition = (namePerson == "Артем") ? console.log("директор") :
+    (namePerson == "Александр") ? console.log("преподаватель") : console.log("студент");
