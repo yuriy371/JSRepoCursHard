25361a87a3b6af0e69526072986858a72d1dde6d
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-06 00:53:15 +0400

    функция проверки строки

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+"use strict"
+const textExam = function (text) {
+    if (typeof text === "string") {
+        if (text.length > 30) {
+            text = text.trim().substring(0, 30) + "...";
+            return(text);
+        } else {
+            return(text.trim());
+        }
+    } else {
+        return("не строка");
+    }
+}
 
+console.log(textExam(" абзацабзацабзацабзацабзацабзац ")); // больше 30 символов
+console.log(textExam(" абзацабзацабзацабзацабзац ")); // пустые символы
+console.log(textExam(15)); // число
