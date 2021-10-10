3b8c9231b8f14e742c82ad2e5b1095847cf00fa8
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-10 19:47:52 +0400

    вывод дней недели на экран исправдение переменой date

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье",]
-let date = new Date().getUTCDay()
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье",];
+let date = (6 + new Date().getDay()) % 7 // приведение результата getDay к индексам массива
 
 /* --------------------------------- */
 week.forEach(function (item, i) {
@@ -11,10 +11,10 @@ week.forEach(function (item, i) {
         // html = "<i>" + html + "</i>"; //курсив у выходных (с помощью тегов)
     }
     if (i === date) {
+        
         html = html.bold(); // жирный шрифт текущего дня
         // html = "<b>" + html + "</b>";// жирный шрифт текущего дня (с помощью тегов)
     }
-
     let div = document.createElement('div');
     div.innerHTML = html;
     document.body.appendChild(div);
