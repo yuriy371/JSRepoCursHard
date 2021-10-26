f189f7bc279e139e212ee4d2ae591ff053d637a9
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-26 16:03:53 +0400

    задержка выводаЭ

--- script.js
+++ script.js
@@ -1,52 +1,11 @@
 "use strict"
 
-let GetNoun = function (number, one, two, five) {
-    number = Math.abs(number);
-    number %= 100;
-    if (number >= 5 && number <= 20) {
-        return five;
-    }
-    number %= 10;
-    if (number == 1) {
-        return one;
-    } else if (number >= 2 && number <= 4) {
-        return two;
-    } else {
-        return five;
-    }
+let input = document.querySelector(".input")
+let textP = document.querySelector(".text-p")
 
-}
-
-let pad = function (n) {
-    if (n < 10)
-        return "0" + n;
-    return n;
-}
-
-let updateTime = function () {
-    let date = new Date()
-    for (let i = 0; i < 2; i++) {
-        if (i === 0) {
-            let options = {
-                month: 'long',
-                day: 'numeric',
-                weekday: 'long',
-            };
-            let hours = date.getHours() + " " + GetNoun(date.getHours(), "час ", "часа ", "часов ") + date.getMinutes() + " минут " + date.getSeconds() + " секунд"
-            document.getElementById("form1").innerHTML = date.toLocaleString("ru", options) + " " + date.getFullYear() + " года " + hours;
-        } else {
-            let options = {
-                year: 'numeric',
-                month: 'numeric',
-                day: 'numeric',
-                timezone: 'UTC',
-                hour: 'numeric',
-                minute: 'numeric',
-                second: 'numeric'
-            };
-            document.getElementById("form2").innerHTML = pad(date.toLocaleString("ru", options));
-        }
-    }
-}
-setInterval(updateTime, 1000)
-
+input.addEventListener("keyup", () => {
+    let setText = setInterval( () => {
+        textP.innerHTML = input.value
+        clearTimeout(setText)
+    } , 300)
+})
