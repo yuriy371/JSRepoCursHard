36908509eb8782e090070a8e629d446187249d81
Yuriy Nazarov <yuriy-nazarov-2014@mail.ru>
2021-10-26 16:57:43 +0400

    запуск, остановка и сброс анимации

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 "use strict"
 
-let input = document.querySelector(".input")
-let textP = document.querySelector(".text-p")
+let circle = document.querySelector(".circle")
+let btnClick = document.querySelector(".click")
+let reset = document.querySelector(".reset")
 
-input.addEventListener("keyup", () => {
-    let setText = setInterval( () => {
-        textP.innerHTML = input.value
-        clearTimeout(setText)
-    } , 300)
+let active = false
+let count = 0
+let idInterval
+
+let downAnimation = () => {
+    count++
+    idInterval = requestAnimationFrame(downAnimation)
+
+    if (count <= 1000) {
+        circle.style.left = count + "px"
+    } else {
+        cancelAnimationFrame(idInterval)
+    }
+}
+
+btnClick.addEventListener("click", () => {
+    if (active) {
+        cancelAnimationFrame(idInterval)
+        active = false
+    } else {
+        idInterval = requestAnimationFrame(downAnimation)
+        active = true
+    }
+})
+reset.addEventListener("click", () => {
+    cancelAnimationFrame(idInterval)
+    active = false
+    count = 0
+    circle.style.left = ""
 })
